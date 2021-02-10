8378db9cab58b1f687f8a9c88fe97bf745890542
yuben98 <ayubenla98@gmail.com>
2021-02-09 20:42:25 -0500

    fix sound

--- digital1/js/main.js
+++ digital1/js/main.js
@@ -106,7 +106,7 @@ var GameScene = new Phaser.Class({
     eatGabi: function (player, gabi)
     {
         gabi.disableBody(true, true);
-        this.chew.play();
+        (sound)(this.chew).play();
         var h=this.player.displayHeight;
         var w=this.player.displayWidth;
         this.player.setDisplaySize((w+5),(h+5));
